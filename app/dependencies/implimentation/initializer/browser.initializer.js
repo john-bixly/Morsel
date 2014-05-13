004f4bebdb872d0239f267d7723b40ff195e1186
Cooper Hilscher <cooper.hilscher@thinksolid.com>
2014-05-12 22:51:13 -0700

    Cleaned up main, moved initializer functionality into depenedency

--- app/dependencies/implimentation/initializer/browser.initializer.js
+++ app/dependencies/implimentation/initializer/browser.initializer.js
@@ -1,5 +1,5 @@
-define(['facade', 'dependencies/definition/initializer'],
-    function (Facade, initializer) {
+define(['facade', 'dependencies/definition/initializer', 'router', 'underscore', 'backbone', 'jquery', 'foundation'],
+    function (Facade, initializer, Router, _, Backbone, $) {
         'use strict';
         var imp = {
             initialize: initialize
@@ -8,7 +8,39 @@ define(['facade', 'dependencies/definition/initializer'],
         return new Facade(initializer, imp);
 
         function initialize() {
+            _setupRouter.call(this);
+            _setupHistory.call(this);
+            _startUI.call(this);
+        }
 
+        function _setupRouter() {
+            var router = new Router();
+            router.start();
+            _pushStateCompensation.call(this, router);
+        }
+
+        function _pushStateCompensation(router) {
+            $(document).on('click', 'a[href^="/"]', function (event) {
+                if (!event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey) {
+                    if (!$(event.currentTarget).attr('ignoreRouting')) {
+                        event.preventDefault();
+                        var url = $(event.currentTarget).attr('href').replace(/^\//, '');
+                        router.navigate(url, { trigger: true });
+                    }
+                }
+            });
+        }
+
+        function _setupHistory() {
+            Backbone.history.start({pushState: true});
+            Backbone.history.breadCrumb = [];
+            Backbone.history.on('route', function () {
+                Backbone.history.breadCrumb.push(this.fragment);
+            });
+        }
+
+        function _startUI() {
+            $(document).foundation();
         }
 
 
